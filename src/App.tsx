25c782a30b9ed2d042dabff0607317b7e708a94d
Felipe Maruyama <felipe-eiji@hotmail.com>
2020-11-22 17:43:46 -0300

    Adicionar MenuBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
+
+import MenuBar from './component/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App(): React.ReactElement {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
